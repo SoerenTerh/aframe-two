96b7be3cba57978b5755b7983e730e94104cf04b
Melanie Brüssel <mbruess1@smail.uni-koeln.de>
2016-12-08 01:11:47 +0100

    add Projekthomepage

---
+++ website/script/script.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+    "use strict";
+
+    function hide() {
+        $(".oneInfo").addClass("hide");
+        $(".twoInfo").addClass("hide");
+        $(".threeInfo").addClass("hide");
+        $(".fourInfo").addClass("hide");
+        $(".fiveInfo").addClass("hide");
+        $(".sixInfo").addClass("hide");
+        $(".sevenInfo").addClass("hide");
+        $(".eightInfo").addClass("hide");
+    }
+
+    function color(x) {
+        $(".primary-color").css("background-color", "turquoise");
+        $(".no-color").css("background-color", "inherit");
+        $(x).css("background-color", "orange");
+    }
+
+    $(".one").on('click mouseover', function () {
+        color(this);
+        if ($(".oneInfo").hasClass("hide")) {
+            hide();
+            $(".oneInfo").removeClass("hide");
+        }
+    });
+    $(".two").on('click mouseover', function () {
+        color(this);
+        if ($(".twoInfo").hasClass("hide")) {
+            hide();
+            $(".twoInfo").removeClass("hide");
+        }
+    });
+    $(".three").on('click mouseover', function () {
+        color(this);
+        if ($(".threeInfo").hasClass("hide")) {
+            hide();
+            $(".threeInfo").removeClass("hide");
+        }
+    });
+    $(".four").on('click mouseover', function () {
+        color(this);
+        if ($(".fourInfo").hasClass("hide")) {
+            hide();
+            $(".fourInfo").removeClass("hide");
+        }
+    });
+    $(".five").on('click mouseover', function () {
+        color(this);
+        if ($(".fiveInfo").hasClass("hide")) {
+            hide();
+            $(".fiveInfo").removeClass("hide");
+        }
+    });
+    $(".six").on('click mouseover', function () {
+        color(this);
+        if ($(".sixInfo").hasClass("hide")) {
+            hide();
+            $(".sixInfo").removeClass("hide");
+        }
+    });
+    $(".seven").on('click mouseover', function () {
+        color(this);
+        if ($(".sevenInfo").hasClass("hide")) {
+            hide();
+            $(".sevenInfo").removeClass("hide");
+        }
+    });
+    $(".eight").on('click mouseover', function () {
+        color(this);
+        if ($(".eightInfo").hasClass("hide")) {
+            hide();
+            $(".eightInfo").removeClass("hide");
+        }
+    });
+
+});
