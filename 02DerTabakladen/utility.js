58d78c21edfb620341d59f90c461d1373b38f999
Sören <soerenterhorst@yahoo.de>
2016-12-18 12:07:50 +0100

    transition ~ innen

--- 02DerTabakladen/utility.js
+++ 02DerTabakladen/utility.js
@@ -4,7 +4,12 @@
 
 /* außen */
 
-
+document.querySelector('#tabakladen').addEventListener('click', function () {
+    var innen = "indemladen.html";
+    $( '#außen' ).fadeOut( "slow", function() {
+        window.location = innen;
+    });
+})
 
 
 
