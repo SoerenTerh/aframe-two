ab8c18bd231d7908e3cc6689e8e55bee0d733ed0
Melanie Brüssel <mel-bruessel@web.de>
2016-12-22 02:33:46 +0100

    change event from click to mouseenter
    Problem: curser muss türanimation folgen

--- 02DerTabakladen/utility_indoor.js
+++ 02DerTabakladen/utility_indoor.js
@@ -3,10 +3,24 @@
  */
 /* innen */
 
-document.querySelector('#tabakladenTUERi').addEventListener('click', function () {
+/*document.querySelector('#tabakladenTUERi').addEventListener('hover', function () {
     var out = "index.html";
     setTimeout(function () {
         $( '#innen' ).fadeOut( "slow", function () {
             window.location = out;
         })}, 2750);
+});*/
+
+var timeoutId = null;
+
+document.querySelector('#tabakladenTUERi').addEventListener('mouseenter', function () {
+    var out = "index.html";
+    timeoutId = setTimeout(function () {
+        $( '#innen' ).fadeOut( "slow", function () {
+            window.location = out;
+        })}, 2750);
+});
+
+document.querySelector('#tabakladenTUERi').addEventListener('mouseleave', function () {
+    window.clearTimeout(timeoutId)
 });
