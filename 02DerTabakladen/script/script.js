199e5a03a3700346912e811b82e5b49e32f4dd99
Sören <soerenterhorst@yahoo.de>
2017-01-16 22:34:52 +0100

    Scaling of ground plane and 'Teppichladen'. First interior for 'Teppichladen' put into the scene - minor fix to the rotation value of an asset

--- 02DerTabakladen/script/script.js
+++ 02DerTabakladen/script/script.js
@@ -1,6 +1,4 @@
 $(".clickable").each(function () {
     $(this).attr('event-animate', 'target:#cursor; event:clickableFound');
-    setTimeout(function () {
         $(this).attr('event-animate', 'target:#cursor; event:clickableClick');
-    }, 1500);
 });
