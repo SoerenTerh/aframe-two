416f7e25da7f653e28262dfc2a33ff165717fe8f
Sören <soerenterhorst@yahoo.de>
2017-02-05 14:46:40 +0100

    fix: rest of page 2

--- 02DerTabakladen/script/scriptInside.js
+++ 02DerTabakladen/script/scriptInside.js
@@ -15,7 +15,7 @@ var eventArr = ["one", "two",
                 "three", "four", // drin lassen für Schreiner und Shui Ta im Raum
                 "fourTalk", "fourTalk2", "five", "fiveAll", "fiveTalk2", "six", "sixTalk", "seven", "sevenTalk",
                 "sOneTalk", "sTwoTalk", "sThreeTalk", "sFourTalkStart",
-                "sFourTalk1", "sFourTalk2", "sFourP3begin", "sFour_P3", "sFiveTalk", "sFiveTalk2", "sFive_P1", "sFive_P2", "sFiveTalk3", "sFive_P3",
+                "sFourTalk1", "sFourTalk2", "sFourP3begin", "sFour_P3", "sFiveTalk", "sFiveTalk2", "sFive_P1", "sFive_P2", "sFive_P3", "sFiveTalk3",
                 "sFive_P4", "sSixTalk1", "sSixTalk2", "sSixTalk3", "sSixTalk4", "sSeven_P1", "sSeven_P22", "sSeven_P2",
                 "vOneTalk",
                 "vTwo",
@@ -120,8 +120,8 @@ var one = ["#kerzeFlamme", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großva
 
   sFiveTalk = ["#ShuiTa"],
   sFiveTalk2 = ["#Neffe"],
-  sFive_P1 = ["#Neffe", "#Bruder", "#tabakladenTUERi"], // Bewegen an Ladentisch vorbei
-  sFive_P2 = ["#Neffe", "#Bruder"], // Bewegen Richtung Türe vorbei
+  sFive_P1 = ["#Neffe", "#Bruder"], // Bewegen an Ladentisch vorbei
+  sFive_P2 = ["#Neffe", "#Bruder", "#tabakladenTUERi"], // Bewegen Richtung Türe vorbei
   sFive_P3 = ["#Neffe", "#Bruder", "#Schwaegerin", "#Nichte"],
   sFiveTalk3 = ["#Schwaegerin"],
   sFive_P4 = ["#Schwaegerin", "#Nichte", "#tabakladenTUERi"], // Drehen vor die Türe - Türe auf
