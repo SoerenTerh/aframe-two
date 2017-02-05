6e26a6a27ebd9e8db0bf83a1b8711b44bec3a35e
Sören <soerenterhorst@yahoo.de>
2017-02-05 23:27:33 +0100

    further progression in fixing problems with story progress

--- 02DerTabakladen/script/scriptInside.js
+++ 02DerTabakladen/script/scriptInside.js
@@ -15,7 +15,8 @@ var eventArr = ["one", "two",
                 "three", "four", // drin lassen für Schreiner und Shui Ta im Raum
                 "fourTalk", "fourTalk2", "five", "fiveAll", "fiveTalk2", "six", "sixTalk", "seven", "sevenTalk",
                 "sOneTalk", "sTwoTalk", "sThreeTalk", "sFourTalkStart",
-                "sFourTalk1", "sFourTalk2", "sFourP3begin", "sFour_P3", "sFiveTalk", "sFiveTalk2", "sFive_P1", "sFive_P2", "sFive_P3", "sFive_P3end", "sFiveTalk3",
+                "sFourTalk1", "sFourTalk2", "sFourP3begin", "sFour_P3", "sFiveTalk", "sFiveTalk2", "sFive_P1", "sFive_P2", "sFive_P3", "sFive_P3no", "sFive_P3s",
+                "sFive_P3end", "sFive_P3end2", "sFiveTalk3",
                 "sFive_P4", "sSixTalk1", "sSixTalk2", "sSixTalk3", "sSixTalk4", "sSeven_P1", "sSeven_P22", "sSeven_P2",
                 "vOneTalk",
                 "vTwo",
@@ -130,10 +131,13 @@ var one = ["#kerzeFlamme", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großva
   sFiveTalk2 = ["#Neffe"],
   sFive_P1 = ["#Neffe", "#Bruder"], // Bewegen an Ladentisch vorbei
   sFive_P2 = ["#Neffe", "#Bruder", "#tabakladenTUERi"], // Bewegen Richtung Türe vorbei
-  sFive_P3 = ["#Neffe", "#Bruder", "#Schwaegerin", "#Nichte"],
+  sFive_P3 = ["#Neffe", "#Bruder", "#Nichte"],
+  sFive_P3no = ["#Nichte"],
+  sFive_P3s = ["#Schwaegerin"],
   sFive_P3end = ["#Schwaegerin"],
+  sFive_P3end2 = ["#Schwaegerin"],
   sFiveTalk3 = ["#Schwaegerin"],
-  sFive_P4 = ["#Schwaegerin", "#Nichte", "#tabakladenTUERi"], // Drehen vor die Türe - Türe auf
+  sFive_P4 = ["#Schwaegerin", "#tabakladenTUERi"], // Drehen vor die Türe - Türe auf
 
   sSixTalk1 = ["#ShuiTa"],
   sSixTalk2 = ["#Mann"],
