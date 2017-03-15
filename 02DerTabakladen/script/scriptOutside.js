fd5d0e1495c66d0b6c99c6deb246b8340c944cb6
Melanie Brüssel <mbruess1@smail.uni-koeln.de>
2017-03-15 21:37:48 +0100

    cleanup js

--- 02DerTabakladen/script/scriptOutside.js
+++ 02DerTabakladen/script/scriptOutside.js
@@ -7,66 +7,21 @@ var fireAt;
 var lastClickableFused;
 var nowClicked;
 
-
-//Arrays
-var eventArr = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty"];
-at = eventArr[0];
-
-var persons = ["#Frau",
-               "#Neffe",
-               "#Mann",
-
-               "#Schwaegerin",
-               "#Bruder",
-
-               "#Großvater",
-               "#Junge",
-
-               "#Nichte",
-
-               "#ShuiTa",
-               "#Schreiner",
-               "#Polizist",
-               "#Hausbesitzerin"];
-
-var one = ["#kerzeFlamme", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", "#Bruder"], //alle schlafen + Lampe brennt
-    two = ["#tabakladenTUERi",  "#ShuiTa", "#Schreiner", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", "#Bruder"], //ShuiTa Klopft (schnarchen stoppt)
-    talkTwo = ["#Frau", "#Neffe"],
-    three = ["#sockelFrau", "#Frau", "#tabakladenTUERi", "#Schreiner", "#ShuiTa"], //Frau öffnet Tür für Schreiner und ShuiTa
-    four = ["#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", "#Bruder"], //alle Wachen auf
-    five = ["#ShuiTa", "#kerzeFlamme"], //ShuiTa geht zu lampe und löscht diese
-    six = ["#ShuiTa"], //ShuiTa Schüttelt Kopf
-    seven = ["#Mann", "#Frau", "#Neffe",  '#Schwaegerin', "#Großvater", "#Junge", "#Bruder"], //Mann organisiert alle (du und du und du....)
-    eight = ["#Junge", "#tabakladenTUERi", "#Frau", "#Neffe", "#Bruder", '#Schwaegerin', "#Alte", "#Großvater"], //Junge nickt und verschindet aus laden (Richtung Bäckerei); alle ziehen sich an
-    nine = ["#Neffe", "#Bruder", '#Schwaegerin', "#Nichte"], //Neffe, Bruder, Schwaegerin & Nichte verlassen Laden
-    ten = ["#ShuiTa", "#bett"], //ShuiTa räumt auf
-    eleven = [],
-    twelve = [],
-    thirteen = [],
-    fourteen = [],
-    fivteen = [],
-    sixteen = [],
-    seventeen = [],
-    eighteen = [],
-    nineteen = [],
-    twenty = [];
-
-//auto-enter VR (https://github.com/aframevr/aframe/issues/1473)
-window.addEventListener('load', function onLoadEnterVR() {
-    'use strict';
-    var scene = document.querySelector('a-scene');
-    if (scene.hasLoaded) {
-        scene.enterVR();
-    } else {
-        el.addEventListener('loaded', function () {
-            scene.enterVR();
-        });
-    }
-});
-
+////auto-enter VR (https://github.com/aframevr/aframe/issues/1473) -> not yet working
+//window.addEventListener('load', function onLoadEnterVR() {
+//    'use strict';
+//    var scene = document.querySelector('a-scene');
+//    if (scene.hasLoaded) {
+//        scene.enterVR();
+//    } else {
+//        el.addEventListener('loaded', function () {
+//            scene.enterVR();
+//        });
+//    }
+//});
 
 //Cursor found .clickable
-$(".clickable").on('fusing', function () {
+$(".clickable").on('fusing', function onFusingClickable() {
     'use strict';
     currentTarget = '#' + $(this).closest("a-entity").attr('id');
     console.log(currentTarget);
@@ -75,12 +30,10 @@ $(".clickable").on('fusing', function () {
     document.querySelector("#cursor").emit(trigggerEvent);
     document.querySelector(currentTarget).emit(trigggerEvent);
     lastClickableFused = currentTarget;
-
-
 });
 
 //Cursor triggers click on .clickable
-$(".clickable").on('click', function () {
+$(".clickable").on('click', function onClickClickable() {
     'use strict';
     if (at !== "two" || at !== "three") {
         currentTarget = '#' + $(this).closest("a-entity").attr('id');
@@ -113,9 +66,8 @@ $(".clickableTrigger").on('fusing', function onClickableTriggerFusing() {
     lastClickableFused = currentTarget;
 });
 
-
 //Cursor is not on .clickable
-$("a-entity").on('fusing', function () {
+$("a-entity").on('fusing', function onFusingEntity() {
     'use strict';
     currentTarget = "#cursor";
     trigggerEvent = "notClickable";
@@ -123,10 +75,10 @@ $("a-entity").on('fusing', function () {
 });
 
 //start storyline
-document.querySelector('a-scene').addEventListener('loaded', function () {
+document.querySelector('a-scene').addEventListener('loaded', function onLoadedScene() {
     'use strict';
 
-    setTimeout(function () {
+    setTimeout(function giveMeTime() {
         $("#giveMeTime").remove();
     }, 2500);
 });
