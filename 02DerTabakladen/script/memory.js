f32876269b9afab005a5dae5b92e4f168f099196
VeraCologne <veracologne@googlemail.com>
2017-05-27 18:02:04 +0200

    add cards to box

---
+++ 02DerTabakladen/script/memory.js
@@ -0,0 +1,151 @@
+var memoryEntity = document.querySelector('#memoryGame');
+var card1 = document.createElement('a-entity');
+var card2 = document.createElement('a-entity');
+var card3 = document.createElement('a-entity');
+var card4 = document.createElement('a-entity');
+var card5 = document.createElement('a-entity');
+var card6 = document.createElement('a-entity');
+var card7 = document.createElement('a-entity');
+var card8 = document.createElement('a-entity');
+var card9 = document.createElement('a-entity');
+var card10 = document.createElement('a-entity');
+var card11 = document.createElement('a-entity');
+var card12 = document.createElement('a-entity');
+var card13 = document.createElement('a-entity');
+var card14 = document.createElement('a-entity');
+var card15 = document.createElement('a-entity');
+var card16 = document.createElement('a-entity');
+var card17 = document.createElement('a-entity');
+var card18 = document.createElement('a-entity');
+var card19 = document.createElement('a-entity');
+
+
+
+AFRAME.utils.entity.setComponentProperty(card1, 'position', {x: 0, y: 0.5, z: 0});
+AFRAME.utils.entity.setComponentProperty(card1, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card1, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card1, 'material', 'color: white');
+
+
+AFRAME.utils.entity.setComponentProperty(card2, 'position', {x: 0, y: 0.505, z: 0});
+AFRAME.utils.entity.setComponentProperty(card2, 'rotation', {x: 0, y: 2, z: 0});
+AFRAME.utils.entity.setComponentProperty(card2, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card2, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card2, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card3, 'position', {x: 0, y: 0.51, z: 0});
+AFRAME.utils.entity.setComponentProperty(card3, 'rotation', {x: 0, y: 4, z: 0});
+AFRAME.utils.entity.setComponentProperty(card3, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card3, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card3, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card4, 'position', {x: 0, y: 0.515, z: 0});
+AFRAME.utils.entity.setComponentProperty(card4, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card4, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card4, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card5, 'position', {x: 0, y: 0.52, z: 0});
+AFRAME.utils.entity.setComponentProperty(card5, 'rotation', {x: 0, y: 2, z: 0});
+AFRAME.utils.entity.setComponentProperty(card5, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card5, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card5, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card6, 'position', {x: 0, y: 0.525, z: 0});
+AFRAME.utils.entity.setComponentProperty(card6, 'rotation', {x: 0, y: 3, z: 0});
+AFRAME.utils.entity.setComponentProperty(card6, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card6, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card6, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card7, 'position', {x: 0, y: 0.53, z: 0});
+AFRAME.utils.entity.setComponentProperty(card7, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card7, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card7, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card8, 'position', {x: 0, y: 0.535, z: 0});
+AFRAME.utils.entity.setComponentProperty(card8, 'rotation', {x: 0, y: 5, z: 0});
+AFRAME.utils.entity.setComponentProperty(card8, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card8, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card8, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card9, 'position', {x: 0, y: 0.54, z: 0});
+AFRAME.utils.entity.setComponentProperty(card9, 'rotation', {x: 0, y: 4, z: 0});
+AFRAME.utils.entity.setComponentProperty(card9, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card9, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card9, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card10, 'position', {x: 0, y: 0.545, z: 0});
+AFRAME.utils.entity.setComponentProperty(card10, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card10, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card10, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card11, 'position', {x: 0, y: 0.55, z: 0});
+AFRAME.utils.entity.setComponentProperty(card11, 'rotation', {x: 0, y: 1, z: 0});
+AFRAME.utils.entity.setComponentProperty(card11, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card11, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card11, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card12, 'position', {x: 0, y: 0.555, z: 0});
+AFRAME.utils.entity.setComponentProperty(card12, 'rotation', {x: 0, y: 3, z: 0});
+AFRAME.utils.entity.setComponentProperty(card12, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card12, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card12, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card13, 'position', {x: 0, y: 0.56, z: 0});
+AFRAME.utils.entity.setComponentProperty(card13, 'rotation', {x: 0, y: 5, z: 0});
+AFRAME.utils.entity.setComponentProperty(card13, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card13, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card13, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card14, 'position', {x: 0, y: 0.565, z: 0});
+AFRAME.utils.entity.setComponentProperty(card14, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card14, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card14, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card15, 'position', {x: 0, y: 0.57, z: 0});
+AFRAME.utils.entity.setComponentProperty(card15, 'rotation', {x: 0, y: 3, z: 0});
+AFRAME.utils.entity.setComponentProperty(card15, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card15, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card15, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card16, 'position', {x: 0, y: 0.575, z: 0});
+AFRAME.utils.entity.setComponentProperty(card16, 'rotation', {x: 0, y: 5, z: 0});
+AFRAME.utils.entity.setComponentProperty(card16, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card16, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card16, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card17, 'position', {x: 0, y: 0.58, z: 0});
+AFRAME.utils.entity.setComponentProperty(card1, 'rotation', {x: 0, y: 3, z: 0});
+AFRAME.utils.entity.setComponentProperty(card17, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card17, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card17, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card18, 'position', {x: 0, y: 0.585, z: 0});
+AFRAME.utils.entity.setComponentProperty(card1, 'rotation', {x: 0, y: 2, z: 0});
+AFRAME.utils.entity.setComponentProperty(card18, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card18, 'scale', {x: 0.17, y: 0.005, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card18, 'material', 'color: white');
+
+AFRAME.utils.entity.setComponentProperty(card19, 'position', {x: 0, y: 0.59, z: 0});
+AFRAME.utils.entity.setComponentProperty(card19, 'geometry', 'primitive: box');
+AFRAME.utils.entity.setComponentProperty(card19, 'scale', {x: 0.17, y: 0.001, z: 0.1});
+AFRAME.utils.entity.setComponentProperty(card19, 'material', 'color: white');
+
+memoryEntity.appendChild(card1);
+memoryEntity.appendChild(card2);
+memoryEntity.appendChild(card3);
+memoryEntity.appendChild(card4);
+memoryEntity.appendChild(card5);
+memoryEntity.appendChild(card6);
+memoryEntity.appendChild(card7);
+memoryEntity.appendChild(card8);
+memoryEntity.appendChild(card9);
+memoryEntity.appendChild(card10);
+memoryEntity.appendChild(card11);
+memoryEntity.appendChild(card12);
+memoryEntity.appendChild(card13);
+memoryEntity.appendChild(card14);
+memoryEntity.appendChild(card15);
+memoryEntity.appendChild(card16);
+memoryEntity.appendChild(card17);
+memoryEntity.appendChild(card18);
+memoryEntity.appendChild(card19);
