c93a66114ca5bf1666030e92399b40925d3c2df2
Sören <soerenterhorst@gmail.com>
2017-06-10 12:33:23 +0200

    win condition + css

--- 02DerTabakladen/script/spiel_verstecken.js
+++ 02DerTabakladen/script/spiel_verstecken.js
@@ -1,7 +1,11 @@
-var xPos = [-33, -14, 7, 25, 43, 43, 43, 78, 82, 14.5, -11.5, -74, -76, -75.5, -5.5];
-var yPos = [-73, -75, -75, -50, -38, -13.5, -2.5, 15, 35.5, 71, 71, 35, 13.5, -17.5, -6];	
+var xPos = [-36, -13.5, 8.5, 25, 43, 43, 43, 78, 82, 14.5, -11.5, -74, -76, -75.5, -5.5];
+var yPos = [-71.5, -73, -71, -50, -38, -13.5, -2.5, 15, 35.5, 71, 71, 35, 13.5, -17.5, -6];	
 var personLeft = 6;
 var personFound = 0;
+
+if($("#HideAndSeek").attr('visible')){
+    $('#counterPerson').css("display", "initial");
+}
 
 updateCounterPerson();
 
@@ -13,6 +17,7 @@ $('.HaSperson').on('click', function(el){
 
     if(personLeft == 0){
         won();
+        $('#counterPerson').css("display", "none");
     }
 });    
 
