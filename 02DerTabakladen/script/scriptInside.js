dc18b400b2d3c1cd22ab7b1d5b7e169bc0ec00b1
VeraCologne <veracologne@googlemail.com>
2017-07-24 20:18:54 +0200

    Add back talk elements
    Probleme siehe GitHub issues, vor allem: mfivetalk2 ist kaputt

--- 02DerTabakladen/script/scriptInside.js
+++ 02DerTabakladen/script/scriptInside.js
@@ -1,4 +1,3 @@
-//Variables
 var at = 0;
 var last = 0;
 var trigggerEvent = 0;
@@ -14,152 +13,79 @@ var currentCursor;
 
 console.log("Neue Version 7:12");
 
-/**
- * Array of events - used in the methods below
- */
-var eventArr = ["one",
-                "two",
-                // "twoTalk", "twoTalk2",
-                "three", "four",
-                //"fourTalk", "fourTalk2",
-                // "fiveAll",
-                "five",
-                //  "sixTalk",
-                "six", "six2",
-                //  "sixTalk2",
-                //    "sevenTalk0",
-                // "seven", "seven2", "seven3", "seven4",
-                //    "sevenTalk",
-                //  "sOneTalk", "sTwoTalk", "sThreeTalk",
-                //"sFourTalkStart",
-                //"sFourTalk1", "sFourTalk2",
+
+// Array of events - used in the methods below
+var eventArr = ["one", "two", "twoTalk", "twoTalk2", "three", "four", "fourTalk", "fourTalk2", "fiveAll", "five",
+                "sixTalk", "six", "six2", "sixTalk2", "sevenTalk0", "seven", "seven2", "seven3", "seven4",
+                "sevenTalk", "sOneTalk", "sTwoTalk", "sThreeTalk", "sFourTalkStart", "sFourTalk1", "sFourTalk2",
                 "sFourACC1", "sFourACC2", "sFourACC3", "sFourACC4", "sFourACC5", "sFourACC6", "sFourACC7", "sFourACC8",
-                "sFourP3begin", "sFourMove", "sFour_P3",
-                //  "sFiveTalk", "sFiveTalk2",
-                "sFive_P1", "sFive_P2", "sFive_P2Move", "sFive_P3", "sFive_P3Move", "sFive_P3no",
-                // "sSixTalk1", "sSixTalk2", "sSixTalk3",
-                "sSeven_P1", "sSeven_P2rot", "sSevenPRotaPU","sSeven_P2", "sSeven_P2end",
-                "vOne",
-                // "vOneTalk",
-                "vTwo",
-                // "vTwoTalk", "vFourTalk",
-                "vFive",
-                // "vSixTalk", "vNineTalk",
-                "vEleven",
-                // "vElevenTalk",
-                "vFifteen",
-                // "vFourteenTalk",
-                "vSixteen",
-                // "vSixteenTalk",
-                "vEighteen", "vEighteen2",
-                "vEighteen3",
-                // "vEighteenTalk",
-                // "v21Talk",
-                "v21", "v21_2",
-                // "v22Talk",
-                "v21_3",
-                // "v23Talk", "v25Talk",
-                "v26", "v26two", "v26three",
-                // "v27Talk",
-                "v28",
-                // "v28Talk", "v29Talk", "v30Talk", "v31Talk",
-                "v32",
-                // "v32Talk",
-                // "v33Talk",
-                // "v34Talk",
-                // "v36Talk",
-                //"mzeroTalk",
-                "mone",
-                //"mtwoTalk",
-                "mthree", //"mthreeTalk",
-                "mfour",
-                //"mfourTalk",
-                "mfive", "mfive2", //"mfiveTalk", "mfiveTalk2",
-                "msix", "msix2",
-                //  "msix2Talk",
-                "mseven", //"msevenTalk", "msevenTalk2", "msevenTalk3",
-                "mseven2", //"msevenTalk4",
-                //"cOneTalk", "cTwoTalk",
-                "cThree", "cThree1", "cFour",
-                //"cFiveTalk",
-                "cFive1", "cSix",
-                "cSix1", "cSix2", "cFive3", "cSeven", "cSevenTalk", "cEight"
+                "sFourP3begin", "sFourMove", "sFour_P3", "sFiveTalk", "sFiveTalk2", "sFive_P1", "sFive_P2",
+                "sFive_P2Move", "sFive_P3", "sFive_P3Move", "sFive_P3no", "sSixTalk1", "sSixTalk2", "sSixTalk3",
+                "sSeven_P1", "sSeven_P2rot", "sSevenPRotaPU","sSeven_P2", "sSeven_P2end", "vOne", "vOneTalk",
+                "vTwo", "vTwoTalk", "vFourTalk", "vFive", "vSixTalk", "vNineTalk", "vEleven", "vElevenTalk",
+                "vFifteen", "vFourteenTalk", "vSixteen", "vSixteenTalk", "vEighteen", "vEighteen2", "vEighteen3",
+                "vEighteenTalk", "v21Talk", "v21", "v21_2", "v22Talk", "v21_3", "v23Talk", "v25Talk", "v26", "v26two",
+                "v26three", "v27Talk", "v28", "v28Talk", "v29Talk", "v30Talk", "v31Talk", "v32", "v32Talk",
+                "v33Talk", "v34Talk", "v36Talk", "mzeroTalk", "mone", "mtwoTalk", "mthree", "mthreeTalk",
+                "mfour", "mfourTalk", "mfive", "mfive2", "mfiveTalk", "mfiveTalk2", "msix", "msix2", "msix2Talk",
+                "mseven", "msevenTalk", "msevenTalk2", "msevenTalk3", "mseven2", "msevenTalk4", "cOneTalk", "cTwoTalk",
+                "cThree", "cThree1", "cFour", "cFiveTalk", "cFive1", "cSix", "cSix1", "cSix2", "cFive3", "cSeven",
+                "cSevenTalk", "cEight"
                ];
 at = eventArr[0];
 
 var persons = ["#Frau",
                "#Neffe",
                "#Mann",
-
                "#Schwaegerin",
-
                "#Großvater",
                "#Junge",
-
                "#Nichte",
-
                "#ShuiTa",
                "#Schreiner",
                "#Polizist"];
 
-/**
- * Different default color for figures
- */
 var personColors = ["#EBD3B9",
-                    "#C5E2E4", 
+                    "#C5E2E4",
                     "#FFD9D9",
-
-                    "#FFE0A8", 
-
+                    "#FFE0A8",
                     "#B99C79",
                     "#D2E6C1",
-
                     "#E08989",
-
-                    "#CACACA", 
+                    "#CACACA",
                     "#EDAE81",
                     "#CDCCF7"];
 
-/**
- * Array of figures to trigger depending on event name - animations and sounds are set as attributes in the corresponding html
- */
-var one = ["#kerzeFlamme", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", /*"#Bruder",*/ "#Nichte"], //alle schlafen + Lampe brennt
-    two = ["#kerzeFlamme", "#tabakladenTUERi",  "#ShuiTa", "#Schreiner", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", /*"#Bruder",*/ "#Nichte"], //ShuiTa Klopft (schnarchen stoppt)
-    // twoTalk = ["#Frau", "#Neffe"],
-    // twoTalk2 = ["#Frau"], //Frau lacht
+// Array of figures to trigger depending on event name - animations and sounds are set as attributes in the corresponding html
+var one = ["#kerzeFlamme", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", "#Nichte"], //alle schlafen + Lampe brennt
+    two = ["#kerzeFlamme", "#tabakladenTUERi",  "#ShuiTa", "#Schreiner", "#Frau", "#Neffe", "#Mann", '#Schwaegerin',
+           "#Großvater", "#Junge", "#Nichte"], //ShuiTa Klopft (schnarchen stoppt)
+    twoTalk = ["#Frau", "#Neffe"],
+    twoTalk2 = ["#Frau"], //Frau lacht
     three = ["#kerzeFlamme", "#sockelFrau", "#containerFrau", "#tabakladenTUERi"], //Frau öffnet Tür für Schreiner und ShuiTa
     four = ["#containerShuiTa", "#containerSchreiner", "#tabakladenTUERi"], //ShuiTa und Schreiner treten ein
-    // fourTalk = ["#ShuiTa", "#Frau"],
-    // fourTalk2 = ["#ShuiTa"],
-    // fiveAll = ["#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", /*"#Bruder",*/ "#Nichte"],
-    five = ["#kerzeFlamme", "#sockelNeffe", "#containerNeffe",
-            "#sockelMann", "#containerMann",
-            '#sockelSchwaegerin', '#containerSchwaegerin',
-            "#sockelGroßvater", "#containerGroßvater",
-            "#sockelJunge", "#containerJunge",
-            /*"#sockelBruder",*/ /*"#containerBruder",*/
-            "#sockelNichte", "#containerNichte"], //alle Wachen auf
-    // sixTalk = ["#Neffe"],
-
+    fourTalk = ["#ShuiTa", "#Frau"],
+    fourTalk2 = ["#ShuiTa"],
+    fiveAll = ["#Neffe", "#Mann", '#Schwaegerin', "#Großvater", "#Junge", "#Nichte"],
+    five = ["#kerzeFlamme", "#sockelNeffe", "#containerNeffe", "#sockelMann", "#containerMann",
+            '#sockelSchwaegerin', '#containerSchwaegerin', "#sockelGroßvater", "#containerGroßvater", "#sockelJunge",
+            "#containerJunge", "#sockelNichte", "#containerNichte"], //alle Wachen auf
+    sixTalk = ["#Neffe"],
     six = ["#containerShuiTa"],
     six2 = ["#containerShuiTa", "#kerzeFlamme"], //ShuiTa geht zu lampe und löscht diese
-    // sixTalk2 = ["#ShuiTa",  "#Mann"],
-    // sevenTalk0 = ["#ShuiTa"],
-    // seven = ["#containerShuiTa"], //ShuiTa Schüttelt Kopf
-    // seven2 = ["#containerShuiTa"], //ShuiTa Schüttelt Kopf
-    // seven3 = ["#containerShuiTa"], //ShuiTa Schüttelt Kopf
-    // seven4 = ["#containerShuiTa"],
-    // sevenTalk = ["#Mann", '#Schwaegerin', "#ShuiTa"],
-    //
-    // sOneTalk = ["#Frau"],
-    // sTwoTalk = ["#Neffe"],
-    // sThreeTalk = ["#Mann"],
-
-    //sFourTalkStart = ["#Mann"],
-    // ADD "Du, du und du" ANIMATIONS HERE!!!
-    //sFourTalk1 = ["#Mann", "#Schwaegerin"],
-    // sFourTalk2 = ["#Mann"],
-    //
+    sixTalk2 = ["#ShuiTa",  "#Mann"],
+    sevenTalk0 = ["#ShuiTa"],
+    seven = ["#containerShuiTa"], //ShuiTa Schüttelt Kopf
+    seven2 = ["#containerShuiTa"], //ShuiTa Schüttelt Kopf
+    seven3 = ["#containerShuiTa"], //ShuiTa Schüttelt Kopf
+    seven4 = ["#containerShuiTa"],
+    sevenTalk = ["#Mann", '#Schwaegerin', "#ShuiTa"],
+    sOneTalk = ["#Frau"],
+    sTwoTalk = ["#Neffe"],
+    sThreeTalk = ["#Mann"],
+    sFourTalkStart = ["#Mann"],
+    sFourTalk1 = ["#Mann", "#Schwaegerin"],
+    sFourTalk2 = ["#Mann"],
     sFourACC1 = ["#accMann"],
     sFourACC2 = ["#accNeffe"],
     sFourACC3 = ["#accFrau"],
@@ -168,94 +94,85 @@ var one = ["#kerzeFlamme", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großva
     sFourACC6 = ["#accNichte"],
     sFourACC7 = ["#accSchwaegerin"],
     sFourACC8 = ["#accSchwaegerin2"],
-
     sFourP3begin = ["#containerJunge", "#tabakladenTUERi"],
     sFourMove = ["#containerJunge"],
     sFour_P3 = [ "#containerJunge", "#tabakladenTUERi"],
-
-    // sFiveTalk = ["#ShuiTa"],
-    // sFiveTalk2 = ["#Neffe"],
+    sFiveTalk = ["#ShuiTa"],
+    sFiveTalk2 = ["#Neffe"],
     sFive_P1 = ["#Neffe", "#Bruder"], // Bewegen an Ladentisch vorbei
     sFive_P2 = ["#Neffe", /*"#Bruder",*/ "#tabakladenTUERi"], // Bewegen Richtung Türe vorbei
     sFive_P2Move = ["#Neffe"/*, "#Bruder"*/],
     sFive_P3 = ["#Neffe", /*"#Bruder",*/ "#Nichte"],
     sFive_P3Move = ["#Nichte"],
     sFive_P3no = ["#Nichte", "#tabakladenTUERi"],
-    // sSixTalk1 = ["#ShuiTa", "#Mann"],
-    // sSixTalk2 = ["#ShuiTa"],
-    // sSixTalk3 = ["#ShuiTa"],
-
+    sSixTalk1 = ["#ShuiTa", "#Mann"],
+    sSixTalk2 = ["#ShuiTa"],
+    sSixTalk3 = ["#ShuiTa"],
     sSeven_P1 = ["#ShuiTa", "#bett", "#bett-2", "#bett-6"], // Shui Ta räumt Schlafplätze vom Boden auf
     sSeven_P2rot = ["#containerShuiTa", "#bett-7", "#bett-8"],
     sSevenPRotaPU = ["#containerShuiTa"],
     sSeven_P2 = ["#containerShuiTa"],
     sSeven_P2end = ["#containerShuiTa", "#bett-3", "#bett-5"], // Shui Ta legt Schlafplätze auf dem Bettgestell ab
-
     vOne = ["#ShuiTa", "#Schreiner", "#Frau", "#Mann"],
-    // vOneTalk = ["#Schreiner"], // Schreiner redet
+    vOneTalk = ["#Schreiner"], // Schreiner redet
     vTwo = ["#ShuiTa", "#Rechnung"], // Rechnung fliegt zu ShuiTa
-    // vTwoTalk = ["#ShuiTa", "#Schreiner"], // zu viel, ernähren
-    // vFourTalk = ["#ShuiTa", "#Schreiner"], // wie viele Kinder
+    vTwoTalk = ["#ShuiTa", "#Schreiner"], // zu viel, ernähren
+    vFourTalk = ["#ShuiTa", "#Schreiner"], // wie viele Kinder
     vFive = ["#Rechnung"], // Rechnung wieder weg
-    // vSixTalk = ["#ShuiTa", "#Mann", "#Schreiner" ], // 20 Dollar, Nussbaum
-    // vNineTalk = ["#ShuiTa", "#Schreiner"], // dann weg, was
+    vSixTalk = ["#ShuiTa", "#Mann", "#Schreiner" ], // 20 Dollar, Nussbaum
+    vNineTalk = ["#ShuiTa", "#Schreiner"], // dann weg, was
     vEleven = ["#Frau"], // Frau aus dem Weg räumen
-    // vElevenTalk = ["#ShuiTa", "#Frau", "#Schreiner"], // zu teuer, gut gegeben, ShenTe soll kommen
+    vElevenTalk = ["#ShuiTa", "#Frau", "#Schreiner"], // zu teuer, gut gegeben, ShenTe soll kommen
     vFifteen = ["#Schreiner"], // geht zu Stellage
-    // vFourteenTalk = ["#ShuiTa"], // Sie ist ruiniert
+    vFourteenTalk = ["#ShuiTa"], // Sie ist ruiniert
     vSixteen = ["#Schreiner", "#ZigarettenkisteRegalLinks", "#stellage_ohne-rechnung"], // nimmt Stellage und trägt zur Tür
-    // vSixteenTalk = ["#Schreiner", "#ShuiTa"], // auf dem Boden, Mann helfen
+    vSixteenTalk = ["#Schreiner", "#ShuiTa"], // auf dem Boden, Mann helfen
     vEighteen = ["#Mann", "#Schreiner"], // umdrehen
     vEighteen2 = ["#Mann"], // zu Stellage
     vEighteen3 = ["#Mann", "#ZigarettenkisteRegalLinksFast", "#stellage_ohne-rechnung-2"], // trägt 2. Stellage zur Tür
-    // vEighteenTalk = ["#Mann", "#Schreiner", "#ShuiTa"], // hinaus, du Hund, 20 Dollar
-    // v21Talk = ["#Schreiner"], // 100
+    vEighteenTalk = ["#Mann", "#Schreiner", "#ShuiTa"], // hinaus, du Hund, 20 Dollar
+    v21Talk = ["#Schreiner"], // 100
     v21 = [ "#Mann", "#tabakladenTUERi"], // Mann trägt Stellage aus Tür raus
     v21_2 = ["#Mann", "#stellage_ohne-rechnung"],
-    // v22Talk = ["#Schreiner", "#ShuiTa", "#Frau"], // nach Maß, 20, quietschen
+    v22Talk = ["#Schreiner", "#ShuiTa", "#Frau"], // nach Maß, 20, quietschen
     v21_3 = [ "#Mann", "#tabakladenTUERi"], // Mann kommt wieder rein
-    // v23Talk = ["#Schreiner", "#ShuiTa"], // legt Münzen auf Tisch
+    v23Talk = ["#Schreiner", "#ShuiTa"], // legt Münzen auf Tisch
     v24 = ["#ShuiTa", "#Mann"], // legt Münzen auf Tisch, Mann trägt 1. Stellage zurück
-    // v25Talk = ["#Mann"], // genug für verschnittene Bretter
+    v25Talk = ["#Mann"], // genug für verschnittene Bretter
     v26 = ["#Mann", "#stellage_ohne-rechnung"], // trägt 2. Stellage zurück: drehen
     v26two = ["#Mann", "#stellage_ohne-rechnung-2"], // trägt 2. Stellage zurück
     v26three = ["#Mann"],
-    // v27Talk = ["#Schreiner"], // zu betrinken
+    v27Talk = ["#Schreiner"], // zu betrinken
     v28 = ["#Schreiner"], // ab
-    // v28Talk = ["#Mann", "#Frau", "#ShuiTa"], // amüsieren, ShuiTa = raus
-    // v29Talk = ["#Mann", "#ShuiTa"], // wir? Diebe
-    // v30Talk = ["#Mann", "#ShuiTa"], // wo Junge?
-
-    // v31Talk = ["#ShuiTa"], // Wie ihr wollt
+    v28Talk = ["#Mann", "#Frau", "#ShuiTa"], // amüsieren, ShuiTa = raus
+    v29Talk = ["#Mann", "#ShuiTa"], // wir? Diebe
+    v30Talk = ["#Mann", "#ShuiTa"], // wo Junge?
+    v31Talk = ["#ShuiTa"], // Wie ihr wollt
     v32 = ["#Schreiner", "#ShuiTa", "#tabakladenTUERi", "#containerPolizist"], // Shui zur Tür, Polizist taucht auf
-    // v32Talk = ["#ShuiTa", "#Polizist"], // Beamter Viertel? Jawohl
-    // v33Talk = ["#ShuiTa"], // "Shui Ta"
-    // v34Talk = ["#ShuiTa", "#Polizist"], // schönes Wetter, bisschen warm ENDE VERA
-    // v36Talk = ["#ShuiTa"],
-
-    //fünfteSeite
-    //mzeroTalk = ["#Mann"], //mann14
-
+    v32Talk = ["#ShuiTa", "#Polizist"], // Beamter Viertel? Jawohl
+    v33Talk = ["#ShuiTa"], // "Shui Ta"
+    v34Talk = ["#ShuiTa", "#Polizist"], // schönes Wetter, bisschen warm ENDE VERA
+    v36Talk = ["#ShuiTa"],
+    mzeroTalk = ["#Mann"], //mann14
     mone = ["#Mann", "#accMann"], //Zeichen an Shui Ta (unbeachtet bei diesem)
-    //  mtwoTalk = ["#ShuiTa", "#Polizist"], //shui27, polizist3
+    mtwoTalk = ["#ShuiTa", "#Polizist"], //shui27, polizist3
     mthree = ["#Frau", "#Mann"], //Frau zu Mann (drehen)
-    //mthreeTalk = ["#Frau", "#ShuiTa"], //frau8, shui28
+    mthreeTalk = ["#Frau", "#ShuiTa"], //frau8, shui28
     mfour = ["#containerShuiTa", "#containerPolizist"], //tritt ein
-    //mfourTalk = ["#Polizist", "#Mann", "#ShuiTa"], //polizist4, mann15, shui29
+    mfourTalk = ["#Polizist", "#Mann", "#ShuiTa"], //polizist4, mann15, shui29
     mfive = ["#containerShuiTa"], //Man (ShuiTa) verbeugt sich
     mfive2 = ["#containerShuiTa"],
-    //  mfiveTalk = ["#ShuiTa", "#Mann"], //shui30, mann16
-    //mfiveTalk2 = ["#ShuiTa", "#asky", "#Polizist"], //von der Straße Lärm und Rufe;;;; shui31, stimmeausdemoff1, polizist5
+    mfiveTalk = ["#ShuiTa", "#Mann"], //shui30, mann16
+    mfiveTalk2 = ["#ShuiTa", "#asky", "#Polizist"], //von der Straße Lärm und Rufe;;;; shui31, stimmeausdemoff1, polizist5
     msix = ["#containerJunge", "#containerEssen"], //Kuch*en* und Flad*en* fallen aus Bluse
     msix2 = ["#containerFrau", "#containerJunge"], //Frau winkt ihn verzeifelt hinaus; Junge wendet sich und will weg
-    //msix2Talk = ["#Polizist"], //polizist6
+    msix2Talk = ["#Polizist"], //polizist6
     mseven = ["#containerPolizist", "#containerJunge"], //Hält Jungen fest
-    //msevenTalk = ["#Polizist", "#Junge"], //polizist7, junge1
-    //msevenTalk2 = ["#Polizist", "#Frau"], //polizist8, frau9
-    //msevenTalk3 = ["#Polizist"], //polizist9,
+    msevenTalk = ["#Polizist", "#Junge"], //polizist7, junge1
+    msevenTalk2 = ["#Polizist", "#Frau"], //polizist8, frau9
+    msevenTalk3 = ["#Polizist"], //polizist9,
     mseven2 = ["#containerShuiTa"], //Timeout -> ShuiTa schweigt
-    //msevenTalk4 = ["#Polizist", "#ShuiTa"], //polizist10, shui32
-
+    msevenTalk4 = ["#Polizist", "#ShuiTa"], //polizist10, shui32
     cOneTalk = ["#Frau"], // Frau redet
     cTwoTalk = ["#ShuiTa", "#Polizist"], //shui, und polizist
     cThree = ["#containerShuiTa"],
@@ -266,7 +183,6 @@ var one = ["#kerzeFlamme", "#Frau", "#Neffe", "#Mann", '#Schwaegerin', "#Großva
     cSix = ["#containerSchwaegerin"],
     cSix1 = ["#containerJunge", "#containerSchwaegerin", "#containerGroßvater"],
     cSix2 = ["#containerJunge", "#containerSchwaegerin", "#containerFrau", "#containerMann"],
-    //werden getrieben und gehen ab
     cFive3 = ["#containerMann", "#containerPolizist", "#containerGroßvater"],
     cSeven = ["#containerGroßvater"],
     cSevenTalk = ["#Großvater"], // Guten Tag
